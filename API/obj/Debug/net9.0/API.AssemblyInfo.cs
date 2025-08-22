3b6fb27a93eef8f86b9c67742cf6d56e42744ffb
tunji ty <tytunji29@gmail.com>
2025-08-22 15:04:09 +0100

    changed the docker path

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b9fb12b5bec74240a9347dc9422b5a210fdfff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55064007fcae712fd1a82d8642b2f5553a96e844")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
